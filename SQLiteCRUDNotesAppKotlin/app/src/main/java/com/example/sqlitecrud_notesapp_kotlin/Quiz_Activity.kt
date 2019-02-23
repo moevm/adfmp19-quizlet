d787ca1b61c31c67cd4221fed8b344263d112b14
bozhkosergei <andrew.glbv1997@gmail.com>
2019-02-23 19:06:47 +0300

    first layout

---
+++ SQLiteCRUDNotesAppKotlin/app/src/main/java/com/example/sqlitecrud_notesapp_kotlin/Quiz_Activity.kt
@@ -0,0 +1,24 @@
+package com.example.sqlitecrud_notesapp_kotlin
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+import kotlinx.android.synthetic.main.activity_main.*
+
+import kotlinx.android.synthetic.main.activity_quiz_.*
+
+class Quiz_Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz_)
+
+        backToMain2.setOnClickListener{
+            println("HELLO")
+            val intent = Intent(this, MainActivity::class.java);
+            startActivity(intent);
+        }
+    }
+
+}
