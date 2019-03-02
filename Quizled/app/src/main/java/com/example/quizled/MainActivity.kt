868f53d686bdd1964753570e39a2fcd2a179d020
bozhkosergei <andrew.glbv1997@gmail.com>
2019-03-02 15:26:27 +0300

    small changes layout

---
+++ Quizled/app/src/main/java/com/example/quizled/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.quizled
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        quizButton.setOnClickListener{
+            println("HELLO")
+            val intent = Intent(this, Word_bank::class.java);
+            startActivity(intent);
+        }
+
+
+        wordsButton.setOnClickListener{
+            println("HELLO2")
+            val intent = Intent(this, Quiz_Activity::class.java);
+            startActivity(intent);
+        }
+    }
+
+
+}
